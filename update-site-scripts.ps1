b433283fc9c34cd0b5770164763fd54ed69d357c
Ryan Robinson <ryan@alliterationapplications.com>
2021-04-01 11:13:51 -0400

    Now demoes combining scripts into one design

--- update-site-scripts.ps1
+++ update-site-scripts.ps1
@@ -9,27 +9,55 @@ Catch [System.InvalidOperationException]
     Connect-SPOService -Url "https://$domain-admin.sharepoint.com"
 }
 
-#Site script: Project
+#Site script: Project (general)
 #If the script already exists, update it, including incremented version number
 #If the script doesn't already exist, create a new one
 $script = Get-Content './project-script.json' -Raw
 $description = 'Site script for base project template'
 if (($script_test = Get-SPOSiteScript | Where-Object 'Title' -eq 'Project').count -gt 0) {
-    $script_id = $script_test.Id
-    Set-SPOSiteScript -Identity $script_id -Version (($script_test.Version)++) -Content $script -Description $description
+    $general_script_id = $script_test.Id
+    Set-SPOSiteScript -Identity $general_script_id -Version (($script_test.Version)++) -Content $script -Description $description
 }
 else {
     $new_script = Add-SPOSiteScript -Title 'Project' -Content $script -Description $description
-    $script_id = $new_script.Id
+    $general_script_id = $new_script.Id
 }
 
-#Site design: Project
+#Site script: Project Type 1
+#If the script already exists, update it, including incremented version number
+#If the script doesn't already exist, create a new one
+$script = Get-Content './project-1-script.json' -Raw
+$description = 'Site script for elements unique to project type 1'
+if (($script_test = Get-SPOSiteScript | Where-Object 'Title' -eq 'Project Type 1').count -gt 0) {
+    $project_1_id = $script_test.Id
+    Set-SPOSiteScript -Identity $project_1_id -Version (($script_test.Version)++) -Content $script -Description $description
+}
+else {
+    $new_script = Add-SPOSiteScript -Title 'Project Type 1' -Content $script -Description $description
+    $project_1_id = $new_script.Id
+}
+
+#Site script: Project Type 2
+#If the script already exists, update it, including incremented version number
+#If the script doesn't already exist, create a new one
+$script = Get-Content './project-1-script.json' -Raw
+$description = 'Site script for elements unique to project type 2'
+if (($script_test = Get-SPOSiteScript | Where-Object 'Title' -eq 'Project Type 2').count -gt 0) {
+    $project_2_id = $script_test.Id
+    Set-SPOSiteScript -Identity $project_2_id -Version (($script_test.Version)++) -Content $script -Description $description
+}
+else {
+    $new_script = Add-SPOSiteScript -Title 'Project Type 2' -Content $script -Description $description
+    $project_2_id = $new_script.Id
+}
+
+#Site design: Project Type 1, which combines the general script and the type 1 script
 #If the design already exists, update it, including an incremented version number
 #If the design doesn't already exist, create a new one
 $description = 'Site design for base project template'
 if (($design_test = Get-SPOSiteDesign | Where-Object 'Title' -eq 'Project').count -gt 0) {
     #Web-Template 64 is for a team site template. Use 68 instead if for communication sites.
-    Set-SPOSiteDesign -Identity $design_test.Id -Version (($design_test.Version)++) -SiteScripts $script_id -Description $description -WebTemplate 64
+    Set-SPOSiteDesign -Identity $design_test.Id -Version (($design_test.Version)++) -SiteScripts $general_script_id,$project_1_id -Description $description -WebTemplate 64
 }
 else {
     Add-SPOSiteDesign -Title 'Project' -SiteScripts $script_id -Description $description
